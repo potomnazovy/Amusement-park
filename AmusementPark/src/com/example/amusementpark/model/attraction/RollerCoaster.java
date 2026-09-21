c15e7b4b82d1e8d8b912e97ed46102bc2aedeb60
potomnazovy <skeleton39876@mail.ru>
2026-09-21 22:06:22 +0300

    RollerCoaster class for park like attraction

---
+++ AmusementPark/src/com/example/amusementpark/model/attraction/RollerCoaster.java
@@ -0,0 +1,72 @@
+package com.example.amusementpark.model.attraction;
+
+public class RollerCoaster extends Attraction
+{
+  private int length; //метры
+  private int maxHeight; //метры
+  private int maxSpeed; //км/ч
+  private int loopsCount; //шт
+
+  public RollerCoaster(int price, int capacity, int extremity, int rideDuration, int minHeight, int minWeight,
+    int openHour, int closeHour, AttractionStatus status, int length, int maxHeight, int maxSpeed, int loopsCount)
+  {
+    super(price, capacity, extremity, rideDuration, minHeight, minWeight, openHour, closeHour, status);
+
+    if (length <= 0)
+    {
+      throw new IllegalArgumentException("Протяженность трассы американских горок должна быть положительной");
+    }
+
+    if (maxHeight <= 0)
+    {
+      throw new IllegalArgumentException("Максимальная высота американских горок должна быть положительной");
+    }
+
+    if (maxSpeed <= 0)
+    {
+      throw new IllegalArgumentException("Максимальная скорость американских горок должна быть положительной");
+    }
+
+    if (loopsCount < 0)
+    {
+      throw new IllegalArgumentException("Количество \"мертвых петель\" не должно быть отрицательным");
+    }
+    this.length = length;
+    this.maxHeight = maxHeight;
+    this.maxSpeed = maxSpeed;
+    this.loopsCount = loopsCount;
+  }
+
+  public int getLength()
+  {
+    return this.length;
+  }
+
+  public int getMaxHeight()
+  {
+    return this.maxHeight;
+  }
+
+  public int getMaxSpeed()
+  {
+    return this.maxSpeed;
+  }
+
+  public int getLoopsCount()
+  {
+    return this.loopsCount;
+  }
+
+  @Override
+  public String getType()
+  {
+    return "Американские горки";
+  }
+
+  @Override
+  public double getThroughput()
+  {
+    int totalCycle = rideDuration + 3;
+    return capacity * (60.0 / totalCycle);
+  }
+}
