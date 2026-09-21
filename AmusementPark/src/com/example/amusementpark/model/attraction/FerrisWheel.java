8444acf58bd7921344809671aca7fb6d0aabea54
potomnazovy <skeleton39876@mail.ru>
2026-09-21 22:07:01 +0300

    FerrisWheel for park like attraction

---
+++ AmusementPark/src/com/example/amusementpark/model/attraction/FerrisWheel.java
@@ -0,0 +1,61 @@
+package com.example.amusementpark.model.attraction;
+
+public class FerrisWheel extends Attraction
+{
+  private int maxHeight;
+  private int cabinsCount;
+  private int cabinsCapacity;
+
+  public FerrisWheel(int price, int capacity, int extremity, int rideDuration, int minHeight, int minWeight,
+    int openHour, int closeHour, AttractionStatus status, int maxHeight, int cabinsCount, int cabinsCapacity)
+  {
+    super(price, capacity, extremity, rideDuration, minHeight, minWeight, openHour, closeHour, status);
+
+    if (maxHeight <= 0)
+    {
+      throw new IllegalArgumentException("Максимальная высота колеса обозрения должна быть положительной");
+    }
+
+    if (cabinsCount <= 0)
+    {
+      throw new IllegalArgumentException("Количество кабинок должно быть положительным числом");
+    }
+
+    if (cabinsCapacity <= 0)
+    {
+      throw new IllegalArgumentException(
+        "В кабинку должны помещаться люди, поэтому количество людей в ней должно быть положительное число");
+    }
+
+    this.maxHeight = maxHeight;
+    this.cabinsCount = cabinsCount;
+    this.cabinsCapacity = cabinsCapacity;
+  }
+
+  public int getMaxHeight()
+  {
+    return this.maxHeight;
+  }
+
+  public int getCabinsCount()
+  {
+    return this.cabinsCount;
+  }
+
+  public int getCabinsCapacity()
+  {
+    return this.cabinsCapacity;
+  }
+
+  @Override
+  public String getType()
+  {
+    return "Колесо обозрения";
+  }
+
+  @Override
+  public double getThroughput()
+  {
+    return cabinsCount * cabinsCapacity * (60.0 / rideDuration);
+  }
+}
